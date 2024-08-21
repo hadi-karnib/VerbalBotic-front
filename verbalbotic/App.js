f7fdba9b71c1bf15bc4175dc7d7c688f6c7b5eb9
hadi-karnib <hadikarnib03@gmail.com>
2024-08-22 01:07:00 +0300

    fixed errors

--- verbalbotic/App.js
+++ verbalbotic/App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import loginScreen from "./screens/loginScreen";
+import loginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 
 const Stack = createNativeStackNavigator();
