02d0b6ae797f470a5252f04e40e06867014de736
hadi-karnib <hadikarnib03@gmail.com>
2024-08-22 23:33:30 +0300

    added test for .env

--- verbalbotic/App.js
+++ verbalbotic/App.js
@@ -4,10 +4,13 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import loginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
+import { API_URL } from "@env"; // Import the environment variable
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  console.log("API URL:", API_URL); // You can now use API_URL in your code
+
   return (
     <NavigationContainer>
       <Stack.Navigator
