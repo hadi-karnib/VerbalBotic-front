0f17b9da6f5cdb46a49c8cbf84f7ad30d9864c6a
hadi-karnib <hadikarnib03@gmail.com>
2024-08-22 23:33:39 +0300

    added configs for .env

--- verbalbotic/babel.config.js
+++ verbalbotic/babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env", // This will be used when importing environment variables
+          path: ".env", // Path to your .env file
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
