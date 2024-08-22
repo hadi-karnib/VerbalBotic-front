f7db3e5729e24d4b855843edb25236d69466d0dc
hadi-karnib <hadikarnib03@gmail.com>
2024-08-22 23:38:44 +0300

    added provider

--- verbalbotic/App.js
+++ verbalbotic/App.js
@@ -4,23 +4,27 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import loginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import { API_URL } from "@env";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Login" component={loginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Login" component={loginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </Provider>
   );
 }
