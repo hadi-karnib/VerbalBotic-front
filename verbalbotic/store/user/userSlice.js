e7c68e5eb81adedf8fc58210515b0572224aa331
hadi-karnib <hadikarnib03@gmail.com>
2024-08-23 00:30:24 +0300

    fixed user slice

--- verbalbotic/store/user/userSlice.js
+++ verbalbotic/store/user/userSlice.js
@@ -1,57 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: null,
-  status: "idle",
+  user: null, // Changed to store the logged-in user's data
+  token: null, // Store the authentication token
+  loading: false,
   error: null,
-  token: null, // Add a token to store the JWT
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    signupUserRequest: (state) => {
-      state.status = "loading";
-      state.error = null;
-    },
-    signupUserSuccess: (state, action) => {
-      state.status = "succeeded";
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-    },
-    signupUserFailure: (state, action) => {
-      state.status = "failed";
-      state.error = action.payload;
-    },
-    updateUserRequest: (state) => {
-      state.status = "loading";
-      state.error = null;
-    },
-    updateUserSuccess: (state, action) => {
-      state.status = "succeeded";
-      state.user = action.payload;
-    },
-    updateUserFailure: (state, action) => {
-      state.status = "failed";
-      state.error = action.payload;
-    },
     loginUserRequest: (state) => {
-      state.status = "loading";
+      state.loading = true;
       state.error = null;
     },
     loginUserSuccess: (state, action) => {
-      state.status = "succeeded";
-      state.user = action.payload.user;
+      state.loading = false;
+      state.user = action.payload.user; // Assuming the response includes the user data
       state.token = action.payload.token;
     },
     loginUserFailure: (state, action) => {
-      state.status = "failed";
+      state.loading = false;
+      state.error = action.payload;
+    },
+    signupUserRequest: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    signupUserSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload.user; // Assuming the response includes the user data
+      state.token = action.payload.token;
+    },
+    signupUserFailure: (state, action) => {
+      state.loading = false;
       state.error = action.payload;
     },
     logoutUser: (state) => {
       state.user = null;
       state.token = null;
+      state.error = null;
+      state.loading = false;
     },
   },
 });
