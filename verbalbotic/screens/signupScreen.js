e8e022adbb563e4108655303903facf9169ea139
hadi-karnib <hadikarnib03@gmail.com>
2024-08-22 22:24:30 +0300

    added styles

--- verbalbotic/screens/signupScreen.js
+++ verbalbotic/screens/signupScreen.js
@@ -88,3 +88,88 @@ export default function SignupScreen({ navigation }) {
     </LinearGradient>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  signupBox: {
+    width: 300,
+    padding: 20,
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  signupText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingLeft: 10,
+    backgroundColor: "#fff",
+  },
+  datePickerButton: {
+    width: "100%",
+    height: 40,
+    justifyContent: "center",
+    borderColor: "#ddd",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingLeft: 10,
+    backgroundColor: "#fff",
+  },
+  datePickerText: {
+    color: "#999",
+  },
+  button: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "#67AFCB",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 5,
+    color: "black",
+    paddingRight: 30, // to ensure the text is never behind the icon
+    backgroundColor: "#fff",
+    marginBottom: 15,
+    width: "100%",
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 5,
+    color: "black",
+    paddingRight: 30, // to ensure the text is never behind the icon
+    backgroundColor: "#fff",
+    marginBottom: 15,
+    width: "100%",
+  },
+});
