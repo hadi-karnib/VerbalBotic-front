466b23add20dcc035b5c4a1a7e9d2836f45b9824
hadi-karnib <hadikarnib03@gmail.com>
2024-08-22 22:21:02 +0300

    added styles

--- verbalbotic/screens/LoginScreen.js
+++ verbalbotic/screens/LoginScreen.js
@@ -32,3 +32,54 @@ export default function LoginScreen({ navigation }) {
     </LinearGradient>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loginBox: {
+    width: 300,
+    padding: 20,
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  loginText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingLeft: 10,
+    backgroundColor: "#fff",
+  },
+  button: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "#67AFCB",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  signupText: {
+    marginTop: 20,
+    color: "#000",
+    fontSize: 14,
+  },
+  signupLink: {
+    color: "#67AFCB",
+    fontWeight: "bold",
+  },
+});
