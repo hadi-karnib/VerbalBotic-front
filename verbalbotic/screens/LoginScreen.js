81c2ba07264d67a2f46cdd035d72a88eec1cf478
hadi-karnib <hadikarnib03@gmail.com>
2024-08-22 22:20:05 +0300

    added text

--- verbalbotic/screens/LoginScreen.js
+++ verbalbotic/screens/LoginScreen.js
@@ -11,7 +11,9 @@ import { LinearGradient } from "expo-linear-gradient";
 export default function LoginScreen({ navigation }) {
   return (
     <LinearGradient colors={["#f3cfd6", "#90c2d8"]} style={styles.container}>
-      <View style={styles.loginBox}></View>
+      <View style={styles.loginBox}>
+        <Text style={styles.loginText}>Login</Text>
+      </View>
     </LinearGradient>
   );
 }
