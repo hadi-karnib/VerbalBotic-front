39c3bd7c54a9de8d3fbad4d8808bc13af4865027
hadi-karnib <hadikarnib03@gmail.com>
2024-08-23 17:59:08 +0300

    created chats Screen

---
+++ verbalbotic/screens/ChatsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const ChatsScreen = () => {
+  return (
+    <View>
+      <Text>Chats Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ChatsScreen;
