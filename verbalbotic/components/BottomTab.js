6a9c52b3b644e129fff77d074fe2e21ab8025700
hadi-karnib <hadikarnib03@gmail.com>
2024-08-23 18:25:49 +0300

    added icons as svg

--- verbalbotic/components/BottomTab.js
+++ verbalbotic/components/BottomTab.js
@@ -3,43 +3,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import ChatsScreen from "../screens/ChatsScreen";
 import ProfileScreen from "../screens/Profile";
-
-// SVG to JSX components
-const HomeIcon = ({ color, size }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill={color}
-  >
-    <path d="M10 20v-6h4v6h5v-8h3l-10-9-10 9h3v8z" />
-  </svg>
-);
-
-const MessageBotIcon = ({ color, size }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill={color}
-  >
-    <path d="M20 7h-3v-2h1v-1h-5v1h1v2h-6v-2h1v-1h-5v1h1v2h-3c-1.104 0-2 .896-2 2v10c0 1.104.896 2 2 2h16c1.104 0 2-.896 2-2v-10c0-1.104-.896-2-2-2zm-3 12h-10v-1h10v1zm0-3h-10v-1h10v1zm3-3h-16v-6h16v6z" />
-  </svg>
-);
-
-const UserIcon = ({ color, size }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill={color}
-  >
-    <path d="M12 12c2.209 0 4-1.791 4-4s-1.791-4-4-4-4 1.791-4 4 1.791 4 4 4zm0 2c-2.67 0-8 1.338-8 4v2h16v-2c0-2.662-5.33-4-8-4z" />
-  </svg>
-);
+import HomeIcon from "../assets/Home.svg"; // Importing the SVG file as a component
+import MessageBotIcon from "../assets/Message Bot.svg"; // Importing the SVG file as a component
+import UserIcon from "../assets/User.svg"; // Importing the SVG file as a component
 
 const Tab = createBottomTabNavigator();
 
@@ -49,11 +15,11 @@ const Tabs = () => {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           if (route.name === "Home") {
-            return <HomeIcon color={color} size={size} />;
+            return <HomeIcon width={size} height={size} fill={color} />;
           } else if (route.name === "Chats") {
-            return <MessageBotIcon color={color} size={size} />;
+            return <MessageBotIcon width={size} height={size} fill={color} />;
           } else if (route.name === "Profile") {
-            return <UserIcon color={color} size={size} />;
+            return <UserIcon width={size} height={size} fill={color} />;
           }
         },
         tabBarActiveTintColor: "#0288D1", // Blue when active
