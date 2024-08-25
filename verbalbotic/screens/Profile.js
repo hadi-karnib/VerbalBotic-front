195cfcef7893999dc223287eeddde2c5d33ad810
hadi-karnib <hadikarnib03@gmail.com>
2024-08-26 00:36:05 +0300

    imported the getSelf function

--- verbalbotic/screens/Profile.js
+++ verbalbotic/screens/Profile.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import { getSelf } from "../store/user/userActions";
+import { useDispatch } from "react-redux";
 const Profile = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getSelf());
+  }, [dispatch]);
   return (
     <View>
       <Text>Profile</Text>
