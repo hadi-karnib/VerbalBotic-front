76581c7f4e15b61611a05aca483c830f81955b7b
hadi-karnib <hadikarnib03@gmail.com>
2024-08-26 00:37:46 +0300

    imported neccessary modules

--- verbalbotic/screens/Profile.js
+++ verbalbotic/screens/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { getSelf } from "../store/user/userActions";
 import { useDispatch } from "react-redux";
