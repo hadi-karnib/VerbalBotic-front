7a29e38050e659de249c0639706529345b223124
hadi-karnib <hadikarnib03@gmail.com>
2024-08-27 23:19:24 +0300

    created basic adminProfile

---
+++ verbalbotic/screens/admin/adminProfile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const AdminProfile = () => {
+  return (
+    <View>
+      <Text>Admin Profile</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AdminProfile;
