f32ee8fb6f3ba7efdf72dcc1c34e5f99832a6bc9
hadi-karnib <hadikarnib03@gmail.com>
2024-08-27 23:11:01 +0300

    made basic homepage

---
+++ verbalbotic/screens/admin/adminHome.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const AdminHome = () => {
+  return (
+    <View style={styles.mainView}>
+      <Text>Admin Home</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f8f8f8",
+  },
+});
+
+export default AdminHome;
