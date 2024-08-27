2bfe2414765d3f71d694c618f0cf3617e81dad0a
hadi-karnib <hadikarnib03@gmail.com>
2024-08-28 00:56:32 +0300

    created basic slice

--- verbalbotic/store/store.js
+++ verbalbotic/store/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
 import chatReducer from "./Chats/chatsSlice";
-
+import childrenReducer from "./children/childSlice";
 const store = configureStore({
   reducer: {
     user: userReducer,
     chats: chatReducer,
+    children: childrenReducer,
   },
 });
 
