e413fd44d220725410b70f961ee518b59577e1e1
hadi-karnib <hadikarnib03@gmail.com>
2024-08-27 23:17:59 +0300

    created bottomTabs

---
+++ verbalbotic/components/adminBottomTabs.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-Admintabs";
+import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
+import HomeScreen from "../screens/HomeScreen";
+import ChatsScreen from "../screens/ChatsScreen";
+import ProfileScreen from "../screens/Profile";
+
+const Tab = createBottomTabNavigator();
+
+const AdminTabs = ({ route }) => {
+  const { streak } = route.params || {};
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Home") {
+            return <MaterialIcons name="home" size={35} color={color} />;
+          } else if (route.name === "Chats") {
+            return <FontAwesome5 name="robot" size={size} color={color} />;
+          } else if (route.name === "Profile") {
+            return <FontAwesome5 name="user" size={size} color={color} />;
+          }
+        },
+        tabBarActiveTintColor: "#0288D1",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: { backgroundColor: "#F5F5F5", paddingTop: 10 },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+        initialParams={{ streak }}
+      />
+      <Tab.Screen
+        name="Chats"
+        component={ChatsScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default AdminTabs;
