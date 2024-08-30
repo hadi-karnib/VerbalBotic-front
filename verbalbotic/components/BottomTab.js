74d72dd0f4424ce7d9b3689dcb5c37a7736ef671
hadi-karnib <hadikarnib03@gmail.com>
2024-08-31 00:06:45 +0300

    fixed styling

--- verbalbotic/components/BottomTab.js
+++ verbalbotic/components/BottomTab.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
+import {
+  MaterialIcons,
+  FontAwesome5,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 import HomeScreen from "../screens/HomeScreen";
 import ChatsScreen from "../screens/ChatsScreen";
 import ProfileScreen from "../screens/Profile";
@@ -15,16 +19,25 @@ const Tabs = ({ route }) => {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           if (route.name === "Home") {
-            return <MaterialIcons name="home" size={35} color={color} />;
+            return (
+              <MaterialCommunityIcons name="home" size={35} color={color} />
+            );
           } else if (route.name === "Chats") {
             return <FontAwesome5 name="robot" size={size} color={color} />;
           } else if (route.name === "Profile") {
-            return <FontAwesome5 name="user" size={size} color={color} />;
+            return (
+              <MaterialCommunityIcons
+                name="account-outline"
+                size={35}
+                color={color}
+              />
+            );
           }
         },
         tabBarActiveTintColor: "#0288D1",
         tabBarInactiveTintColor: "gray",
         tabBarStyle: { backgroundColor: "#F5F5F5", paddingTop: 10 },
+        tabBarLabelStyle: { MarginTop: 5, marginBottom: -5 },
       })}
     >
       <Tab.Screen
