dcaff7dba3a132de536b89792862e3423563d350
hadi-karnib <hadikarnib03@gmail.com>
2024-09-08 00:25:35 +0300

    added getSelf

--- verbalbotic/screens/admin/adminchildren.js
+++ verbalbotic/screens/admin/adminchildren.js
@@ -14,6 +14,7 @@ import LottieView from "lottie-react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import NoChildrenAnimation from "../../assets/NoChildren.json";
+import { getSelf } from "../../store/user/userActions";
 
 const Adminchildren = () => {
   const dispatch = useDispatch();
