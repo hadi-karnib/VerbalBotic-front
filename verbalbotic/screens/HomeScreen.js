1dcbe196f4cee610f22986668c6494a54c3282e1
hadi-karnib <hadikarnib03@gmail.com>
2024-09-12 01:37:53 +0300

    added waveform instead of mic

--- verbalbotic/screens/HomeScreen.js
+++ verbalbotic/screens/HomeScreen.js
@@ -1,12 +1,12 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   View,
   Text,
   StyleSheet,
   SafeAreaView,
   TouchableOpacity,
+  Animated,
 } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Audio } from "expo-av";
 import * as FileSystem from "expo-file-system";
@@ -14,7 +14,6 @@ import { useDispatch } from "react-redux";
 import { saveVoiceNote } from "../store/Chats/chatsActions";
 import SwitchToggle from "react-native-switch-toggle";
 import { Dropdown } from "react-native-element-dropdown";
-import { Ionicons } from "@expo/vector-icons";
 
 const HomeScreen = ({ navigation, route }) => {
   const { streak } = route.params || {};
@@ -32,29 +31,15 @@ const HomeScreen = ({ navigation, route }) => {
   const [selectedLanguage, setSelectedLanguage] = useState(null);
 
   const recordingInterval = useRef(null);
+  const waveAnim1 = useRef(new Animated.Value(1)).current;
+  const waveAnim2 = useRef(new Animated.Value(1)).current;
+  const waveAnim3 = useRef(new Animated.Value(1)).current;
 
   const languages = [
     { label: "English (United States)", value: "en-US" },
-    { label: "English (Australia)", value: "en-AU" },
-    { label: "English (Canada)", value: "en-CA" },
-    { label: "English (United Kingdom)", value: "en-GB" },
     { label: "Arabic (Lebanese)", value: "ar-LB" },
-    { label: "Arabic (Algeria)", value: "ar-DZ" },
-    { label: "Arabic (Bahrain)", value: "ar-BH" },
-    { label: "Chinese (Mandarin)", value: "zh" },
-    { label: "Arabic (Egypt)", value: "ar-EG" },
-    { label: "Arabic (Iraq)", value: "ar-IQ" },
-    { label: "Arabic (Kuwait)", value: "ar-KW" },
     { label: "French (France)", value: "fr-FR" },
-    { label: "French (Belgium)", value: "fr-BE" },
-    { label: "French (Canada)", value: "fr-CA" },
-    { label: "French (Switzerland)", value: "fr-CH" },
-    { label: "Chinese (Simplified, China)", value: "cmn-Hans-CN" },
-    { label: "Chinese (Simplified, Hong Kong)", value: "cmn-Hans-HK" },
-    { label: "German (Germany)", value: "de-DE" },
-    { label: "German (Austria)", value: "de-AT" },
-    { label: "German (Switzerland)", value: "de-CH" },
-    { label: "Italian (Italy)", value: "it-IT" },
+    // Other language options...
   ];
 
   const handleToggle = () => {
@@ -64,76 +49,127 @@ const HomeScreen = ({ navigation, route }) => {
     }
   };
 
+  const animateWaves = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(waveAnim1, {
+          toValue: 1.5,
+          duration: 300,
+          useNativeDriver: true,
+        }),
+        Animated.timing(waveAnim1, {
+          toValue: 1,
+          duration: 300,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(waveAnim2, {
+          toValue: 1.8,
+          duration: 400,
+          useNativeDriver: true,
+        }),
+        Animated.timing(waveAnim2, {
+          toValue: 1,
+          duration: 400,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(waveAnim3, {
+          toValue: 2,
+          duration: 500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(waveAnim3, {
+          toValue: 1,
+          duration: 500,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  };
+
+  const stopRecording = async () => {
+    try {
+      await recording.stopAndUnloadAsync();
+      clearInterval(recordingInterval.current);
+
+      const uri = recording.getURI();
+      setRecordingUri(uri);
+
+      const status = await recording.getStatusAsync();
+      const durationInMillis = status.durationMillis || Date.now() - startTime;
+      setDurationMillis(durationInMillis);
+
+      const fileInfo = await FileSystem.getInfoAsync(uri);
+      const sizeInBytes = fileInfo.size;
+      setRecordingSize(sizeInBytes);
+
+      setIsRecording(false);
+      setRecordingText(
+        "Press the microphone button to start recording your voice."
+      );
+
+      const formData = new FormData();
+      formData.append("voiceNote", {
+        uri,
+        name: "voiceNote.m4a",
+        type: "audio/m4a",
+      });
+      formData.append("duration", Math.floor(durationInMillis / 1000));
+      formData.append("format", "m4a");
+      formData.append("size", sizeInBytes);
+
+      dispatch(
+        saveVoiceNote(formData, uri, toggleOn ? selectedLanguage : null)
+      );
+    } catch (error) {
+      console.error("Error stopping recording: ", error);
+    }
+  };
+
+  const startRecording = async () => {
+    try {
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      const recording = new Audio.Recording();
+      await recording.prepareToRecordAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      await recording.startAsync();
+
+      setRecording(recording);
+      setStartTime(Date.now());
+      setIsRecording(true);
+
+      recordingInterval.current = setInterval(async () => {
+        const status = await recording.getStatusAsync();
+        setDurationMillis(status.durationMillis);
+        setRecordingText(
+          `Recording${".".repeat(Math.floor(status.durationMillis / 1000) % 4)}`
+        );
+      }, 1000);
+
+      animateWaves(); // Start the wave animation when recording starts
+    } catch (error) {
+      console.error("Error starting recording: ", error);
+    }
+  };
+
   const handleMicrophonePress = async () => {
     if (isRecording) {
-      try {
-        await recording.stopAndUnloadAsync();
-        clearInterval(recordingInterval.current);
-
-        const uri = recording.getURI();
-        setRecordingUri(uri);
-
-        const status = await recording.getStatusAsync();
-        const durationInMillis =
-          status.durationMillis || Date.now() - startTime;
-        setDurationMillis(durationInMillis);
-
-        const fileInfo = await FileSystem.getInfoAsync(uri);
-        const sizeInBytes = fileInfo.size;
-        setRecordingSize(sizeInBytes);
-
-        setIsRecording(false);
-        setRecordingText(
-          "Press the microphone button to start recording your voice."
-        );
-
-        const formData = new FormData();
-        formData.append("voiceNote", {
-          uri,
-          name: "voiceNote.m4a",
-          type: "audio/m4a",
-        });
-        formData.append("duration", Math.floor(durationInMillis / 1000));
-        formData.append("format", "m4a");
-        formData.append("size", sizeInBytes);
-
-        dispatch(
-          saveVoiceNote(formData, uri, toggleOn ? selectedLanguage : null)
-        );
-      } catch (error) {
-        console.error("Error stopping recording: ", error);
-      }
+      await stopRecording();
     } else {
-      try {
-        await Audio.requestPermissionsAsync();
-
-        await Audio.setAudioModeAsync({
-          allowsRecordingIOS: true,
-          playsInSilentModeIOS: true,
-        });
-
-        const recording = new Audio.Recording();
-        await recording.prepareToRecordAsync(
-          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
-        );
-        await recording.startAsync();
-
-        setRecording(recording);
-        setStartTime(Date.now());
-        setIsRecording(true);
-
-        recordingInterval.current = setInterval(async () => {
-          const status = await recording.getStatusAsync();
-          setDurationMillis(status.durationMillis);
-          setRecordingText(
-            `Recording${".".repeat(
-              Math.floor(status.durationMillis / 1000) % 4
-            )}`
-          );
-        }, 1000);
-      } catch (error) {
-        console.error("Error starting recording: ", error);
-      }
+      await startRecording();
     }
   };
 
@@ -195,17 +231,24 @@ const HomeScreen = ({ navigation, route }) => {
         )}
         <View style={styles.microphoneContainer}>
           <TouchableOpacity
-            style={[
-              styles.microphoneButton,
-              isRecording && styles.recordingButton,
-            ]}
+            style={styles.microphoneButton}
             onPress={handleMicrophonePress}
           >
-            <MaterialIcons
-              name="mic"
-              size={100}
-              color={isRecording ? "#FF5F52 " : "#03A9F4"}
-            />
+            {!isRecording ? (
+              <MaterialIcons name="mic" size={100} color="#FFFFFF" />
+            ) : (
+              <View style={styles.waveformContainer}>
+                <Animated.View
+                  style={[styles.wave, { transform: [{ scaleY: waveAnim1 }] }]}
+                />
+                <Animated.View
+                  style={[styles.wave, { transform: [{ scaleY: waveAnim2 }] }]}
+                />
+                <Animated.View
+                  style={[styles.wave, { transform: [{ scaleY: waveAnim3 }] }]}
+                />
+              </View>
+            )}
           </TouchableOpacity>
         </View>
         <Text style={styles.analyzingText}>
@@ -217,6 +260,7 @@ const HomeScreen = ({ navigation, route }) => {
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
@@ -238,7 +282,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    backgroundColor: "#FFF3E0", // Lighter peach
+    backgroundColor: "#FFF3E0",
     margin: 20,
     padding: 15,
     borderRadius: 10,
@@ -246,19 +290,19 @@ const styles = StyleSheet.create({
   dailyStreakText: {
     fontSize: 18,
     fontWeight: "bold",
-    color: "#FF8A65", // Medium coral
+    color: "#FF8A65",
   },
   streakBox: {
     width: 60,
     height: 60,
-    backgroundColor: "#FFE0B2", // Lighter peach
+    backgroundColor: "#FFE0B2",
     borderRadius: 30,
     justifyContent: "center",
     alignItems: "center",
   },
   streakNumber: {
     fontSize: 30,
-    color: "#E64A19", // Bold orange-red
+    color: "#E64A19",
     fontWeight: "bold",
   },
   toggleContainer: {
@@ -277,19 +321,13 @@ const styles = StyleSheet.create({
     height: 30,
     borderRadius: 25,
     padding: 5,
-    backgroundColor: "#64b5f6", // Brighter sky blue when active
+    backgroundColor: "#64b5f6",
   },
   toggleCircle: {
     width: 20,
     height: 20,
     borderRadius: 10,
-    backgroundColor: "#42a5f5", // Darker blue when active
-  },
-  toggleOffSwitch: {
-    backgroundColor: "#ffab91", // Soft peach when inactive
-  },
-  toggleOffCircle: {
-    backgroundColor: "#ff7043", // Bright orange when inactive
+    backgroundColor: "#42a5f5",
   },
   dropdownContainer: {
     marginHorizontal: 20,
@@ -331,13 +369,23 @@ const styles = StyleSheet.create({
   microphoneButton: {
     width: 150,
     height: 150,
-    backgroundColor: "#0277BD", // Light blue for calmness
+    backgroundColor: "#0277BD",
     borderRadius: 75,
     justifyContent: "center",
     alignItems: "center",
   },
-  recordingButton: {
-    backgroundColor: "#FFCDD2", // Light red for recording
+  waveformContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: 100,
+    alignItems: "center",
+  },
+  wave: {
+    width: 10,
+    backgroundColor: "#FFFFFF",
+    height: 50,
+    marginHorizontal: 5,
+    borderRadius: 5,
   },
   analyzingText: {
     padding: 10,
