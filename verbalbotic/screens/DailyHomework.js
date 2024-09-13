e56dceab03b0e64a3125a1252ed065289f100b85
hadi-karnib <hadikarnib03@gmail.com>
2024-09-13 16:58:06 +0300

    cfreated basic dhw

---
+++ verbalbotic/screens/DailyHomework.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const DailyHomework = () => {
+  return (
+    <View>
+      <Text>Homework</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default DailyHomework;
